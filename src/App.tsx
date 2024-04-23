ffc7b1fc5fe67dbab49593f4fb9bd3c1ff99c352
jdichh <jpsdichoso21@gmail.com>
2024-04-23 11:09:55 +0800

    style: poppins font now from google fonts; other: testing text loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 // src/App.tsx
+import { Html, useProgress } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useState } from "react";
 import Experience from "./components/Experience/Experience";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 import LandingMenu from "./components/UI/LandingMenu/LandingMenu";
 import { MenuContext } from "./lib/contexts/contexts";
+import { Perf } from "r3f-perf";
+
+// replace/improve this
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html
+      center
+      className="w-screen h-screen p-8 flex flex-col justify-center items-center"
+    >
+      <p className="text-white text-xl">{progress.toFixed(0)}%</p>
+    </Html>
+  );
+};
 
 const App = () => {
   const [isInLandingMenu, setIsInLandingMenu] = useState(true);
@@ -17,11 +31,10 @@ const App = () => {
         ) : (
           <Canvas
             shadows
-            style={{ background: "black" }}
+            style={{ background: "#black" }}
             eventSource={document.getElementById("root") as HTMLElement}
           >
-             {/* <Perf position={'bottom-right'} /> */}
-            <Suspense fallback={<LoadingSpinner />}>
+            <Suspense fallback={<Loader />}>
               <Experience />
             </Suspense>
           </Canvas>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ["Poppins", "sans-serif"],
         'fjalla': ["Fjalla One", "sans-serif"]
       }
     },
